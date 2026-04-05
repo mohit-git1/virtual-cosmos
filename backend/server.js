88ddeb22298f815103999015ec28a03fd2487065
mohit-git1 <mohitsharma2826@gmail.com>
2026-04-05 13:57:25 +0530

    added : user connectionvia socket, sending existing users to new user and tell other if someone joined

--- backend/server.js
+++ backend/server.js
@@ -17,3 +17,27 @@ const io = new Server(server, {
 server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+const users = {}; // storing socketID  x and y postions
+
+io.on("connection" , (socket) =>{
+    console.log("User is Connected: ", socket.id);
+        users[socket.id] = {
+            x: 100,
+            y:100
+
+        };
+
+    socket.emit("currentUsers", users);
+    socket.broadcast.emit("newUser", {
+        socket.broadcast.emit("newYuser",
+            {
+                id: socket.id,
+                x:100,
+                y:100
+            }
+        );
+
+    });
+
+
+})
